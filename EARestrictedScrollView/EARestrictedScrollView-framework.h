ab8c5e926190817c8ea2d7231b664f4fd0a7e7eb
Yasuyuki Inoue <yasu@net-walker.jp>
2015-11-02 20:07:35 +0900

    Carthage compatible

---
+++ EARestrictedScrollView/EARestrictedScrollView-framework.h
@@ -0,0 +1,21 @@
+//
+//  EARestrictedScrollView-framework.h
+//  EARestrictedScrollView
+//
+//  Created by Yasuyuki Inoue on 2015/11/02.
+//  Copyright © 2015年 Y.Inoue. All rights reserved.
+//
+
+#ifndef EARestrictedScrollView_framework_h
+#define EARestrictedScrollView_framework_h
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlatUIColors.
+FOUNDATION_EXPORT double EARestrictedScrollViewVersionNumber;
+
+//! Project version string for FlatUIColors.
+FOUNDATION_EXPORT const unsigned char EARestrictedScrollViewVersionString[];
+
+
+#endif /* EARestrictedScrollView_framework_h */
